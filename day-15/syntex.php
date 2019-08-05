e84ab14ab281e4a336b1a82b58f6c2e0c15d232e
Imran423 <greendatacenterltd@gmail.com>
2019-08-05 14:50:59 +0600

    PHP first day-15

---
+++ day-15/syntex.php
@@ -0,0 +1,122 @@
+<?php
+//    echo "<html>";
+//        echo "<head>";
+//            echo "<title>";
+//                echo "Home";
+//            echo "</title>";
+//        echo "</head>";
+//
+//        echo "<body>";
+//            echo "<h1>This is a Heading</h1>";
+//            echo "<p>This is a Paragraph</p>";
+//        echo "</body>";
+//
+//    echo "</html>";
+//?>
+
+<!--<html>-->
+<!--    <head>-->
+<!--        <title></title>-->
+<!--    </head>-->
+<!---->
+<!--    <body>-->
+<!--        <h1>--><?php //echo "This is a heading."; ?><!--</h1>-->
+<!--        <p>--><?php //echo "This is a paragraph."; ?><!--</p>-->
+<!---->
+<!--    </body>-->
+<!---->
+<!--</html>-->
+
+<?php
+
+    // 3 Major rules for creating a variable
+
+        /*
+            * Start with $ Sign
+            * A-Z, a-z, 0-9
+            * No number in First
+         */
+
+    // 3 standard rules for creating a variable
+
+    /*
+        * Small latter
+        * Meaning full
+        * Readable
+     */
+
+
+//    $firstName = "ImRan";
+//    $lastName = "Hossain";
+//    echo $firstName.' '.$lastName;
+
+
+
+ // { Operator }
+
+    /*
+     * Arethmatic Operator
+        *binary (+, -, *, /, %)
+        *unary (++, --, (-), )
+
+    * Assignment operator
+        (=, +=, -=, *=, /=, %=, =
+
+    * Conditional operator
+        >, >=, <, <=, ==, !=, ===, !==
+
+    *Logical operator
+        &&, ||, !
+     */
+
+
+//    $x=10;
+//    $y=20;
+//    echo $x++; //10
+//    echo '<br>';
+//    echo $x--; //11
+//    echo '<br>';
+//    echo --$x; //9
+//    echo '<br>';
+
+
+            /* STATEMENT
+
+             *  Single line statement
+             *  Conditonal Statement
+                  *IF
+                  *IF Else
+                  *IF Else IF
+                  *Switch
+             * Repeated Statement
+                *For
+                *While
+                * Do While
+                * Foreach
+             *
+             */
+
+
+            $x=10;
+            $y=20;
+            if($x<$y){
+                $z= $x+$y;
+                echo $z;
+            }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
